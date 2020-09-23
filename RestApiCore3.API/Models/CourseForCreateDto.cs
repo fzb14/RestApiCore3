8684c1a894cf6f31e02972e17bf253819995a99f
fzb14 <fzb14@hotmail.com>
2020-09-24 02:33:00 +0800

    custom validation with IValidatableObject

--- RestApiCore3.API/Models/CourseForCreateDto.cs
+++ RestApiCore3.API/Models/CourseForCreateDto.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 
 namespace RestApiCore3.API.Models
 {
-    public class CourseForCreateDto
+    public class CourseForCreateDto : IValidatableObject
     {
         [Required]
         [MaxLength(100)]
         public string Title { get; set; }
         [MaxLength(1500)]
         public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult("Title should be different from Description.", new[] { "CourseForCreateDto" });
+            }
+        }
     }
 }
