6b8cb72a81a2b81f5f9a09a5926b39f832e62e9f
fzb14 <fzb14@hotmail.com>
2020-09-24 03:00:04 +0800

    class-level validation with custom attribute

--- RestApiCore3.API/Models/CourseForCreateDto.cs
+++ RestApiCore3.API/Models/CourseForCreateDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestApiCore3.API.ValidationAttributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace RestApiCore3.API.Models
 {
-    public class CourseForCreateDto : IValidatableObject
+    [CourseTitleMustBeDifferentFromDescription]
+    public class CourseForCreateDto //: IValidatableObject
     {
         [Required]
         [MaxLength(100)]
@@ -14,12 +16,12 @@ namespace RestApiCore3.API.Models
         [MaxLength(1500)]
         public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Title == Description)
-            {
-                yield return new ValidationResult("Title should be different from Description.", new[] { "CourseForCreateDto" });
-            }
-        }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult("Title should be different from Description.", new[] { "CourseForCreateDto" });
+        //    }
+        //}
     }
 }
---
+++ RestApiCore3.API/ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+using RestApiCore3.API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiCore3.API.ValidationAttributes
+{
+    public class CourseTitleMustBeDifferentFromDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var course = (CourseForCreateDto)validationContext.ObjectInstance;
+            if (course.Title == course.Description)
+            {
+                return new ValidationResult("Title should be different from Description.", new[] { nameof(CourseForCreateDto) });
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
