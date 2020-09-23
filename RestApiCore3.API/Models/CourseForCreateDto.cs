925ee863001c8788f542c09ba178fb2e11cc312f
fzb14 <fzb14@hotmail.com>
2020-09-24 02:20:14 +0800

    validate input with data annotations

--- RestApiCore3.API/Models/CourseForCreateDto.cs
+++ RestApiCore3.API/Models/CourseForCreateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace RestApiCore3.API.Models
 {
     public class CourseForCreateDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
-
+        [MaxLength(1500)]
         public string Description { get; set; }
     }
 }
