7cdc75a6f8a6671c03117e9cb9aa2aa0e9ece1d4
fzb14 <fzb14@hotmail.com>
2020-09-24 00:41:25 +0800

    custon model binder

--- RestApiCore3.API/Controllers/AuthorCollectionController.cs
+++ RestApiCore3.API/Controllers/AuthorCollectionController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using RestApiCore3.API.Entities;
+using RestApiCore3.API.Helpers;
 using RestApiCore3.API.Models;
 using RestApiCore3.API.Services;
 using System;
@@ -22,7 +23,17 @@ namespace RestApiCore3.API.Controllers
             this.repository = repository;
             this.mapper = mapper;
         }
-
+        [HttpGet("({ids})", Name ="getauthorcollection")]
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthorCollection(
+            [FromRoute][ModelBinder(BinderType =typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        {
+            if (ids == null)
+                return BadRequest();
+            var authorCollection = repository.GetAuthors(ids);
+            if (ids.Count() != authorCollection.Count())
+                return NotFound();
+            return Ok(mapper.Map<IEnumerable<AuthorDto>>(authorCollection));
+        }
         [HttpPost]
         public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(IEnumerable<AuthorForCreateDto> authorCollection)
         {
@@ -32,8 +43,9 @@ namespace RestApiCore3.API.Controllers
                 repository.AddAuthor(author);
             }
             repository.Save();
-
-            return Ok();
+            var authorDtoCollection = mapper.Map<IEnumerable<AuthorDto>>(authorEntityCollection);
+            var ids = string.Join(",", authorEntityCollection.Select(a => a.Id));
+            return CreatedAtRoute("getauthorcollection",new { ids = ids}, authorDtoCollection);
         }
     }
 }
